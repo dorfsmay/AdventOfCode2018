037eb949fa238b9fb4de1532bad70515afb69258
Yves Dorfsman <yves@zioup.com>
2018-12-01 13:50:55 -0700

    day 1 part 2, first cut

--- 1-Chronal_Calibration-python/chronalcalib.py
+++ 1-Chronal_Calibration-python/chronalcalib.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import logging
+
 
 def get_puzzle_input(file_input):
     data = open(file_input).readlines()
@@ -8,13 +10,32 @@ def get_puzzle_input(file_input):
     data = [ int(x) for x in data if len(x) > 0 ]
     return data
 
-def calculate(data):
+def calculate_resulting_freq(data):
     return sum(data)
+
+def frist_duplicate_freq(data):
+    results = list()
+    runs = 0
+    result = 0
+    while True:
+        runs += 1
+        if runs%20 == 0:
+            print(runs, "iterations")
+        for e in data:
+            result += e
+            if result in results:
+                print()
+                return result, runs
+            else:
+                results.append(result)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('\nMissing data file.\n', file=sys.stderr)
         sys.exit(1)
     data = get_puzzle_input(sys.argv[1])
-    print(calculate(data))
+    resultfreq = calculate_resulting_freq(data)
+    first_dupe = frist_duplicate_freq(data)
+    print("Resulting frequency:", resultfreq)
+    print("First duplicate frequency: {} (in {} runs)\n".format(*first_dupe))
 
--- 1-Chronal_Calibration-python/tests.py
+++ 1-Chronal_Calibration-python/tests.py
@@ -1,10 +1,16 @@
 import chronalcalib
 
 def test_add_2_positive_nums():
-    assert chronalcalib.calculate([2, 3]) == 5
+    assert chronalcalib.calculate_resulting_freq([2, 3]) == 5
 
 def test_add_positive_and_negative_nums():
-    assert chronalcalib.calculate([2, -3]) == -1
+    assert chronalcalib.calculate_resulting_freq([2, -3]) == -1
 
-def test_bunch_of_nums():
-    assert chronalcalib.calculate([1, -5, 7, -9, 100]) == 94
+def test_add_bunch_of_nums():
+    assert chronalcalib.calculate_resulting_freq([1, -5, 7, -9, 100]) == 94
+
+def test_dup_freq_simple():
+    assert chronalcalib.frist_duplicate_freq([10, -4, 100, -50, -50])[0] == 6
+
+def test_dup_freq_need_to_run_twice():
+    assert chronalcalib.frist_duplicate_freq([3, 3, 4, -2, -4])[0] == 10
