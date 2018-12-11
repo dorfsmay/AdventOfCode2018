997e252709d66060de71851727669e21a09ead92
Yves Dorfsman <yves@zioup.com>
2018-12-10 22:41:41 -0700

    another attempt at d9p1

---
+++ 10-The_Stars_Align-python/README.md
@@ -0,0 +1,6 @@
+
+Prints the test sample.
+
+Didn't think that th eletter might only appear after 100s, 1000s of
+iterations... and even less of a trick like looking for a smaller box.
+
---
+++ 9-Marble_Mania-python/tests_python_deque.py
@@ -0,0 +1,21 @@
+from mania_python_deque import high_score
+
+def test_full_1():
+    assert high_score(9, 25) == 32
+
+def test_full_2():
+    assert high_score(10, 1618) == 8317
+
+def test_full_3():
+    assert high_score(13, 7999) == 146373
+
+def test_full_4():
+    assert high_score(17, 1104) == 2764
+
+def test_full_5():
+    assert high_score(21, 6111) == 54718
+
+def test_full_6():
+    assert high_score(30, 5807) == 37305
+
+
